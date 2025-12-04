a29567655a79500a8f045357e322745955823ff5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-12-04 15:00:35 +0100

    Added user information using object

--- intro.js
+++ intro.js
@@ -1,6 +1,17 @@
 const myName = " Taiwo Peace Fiyinfoluwa";
-const myAge = 9
+const myAge = 19;
 const myHobby = 'learning, Teaching';
 const programmingLanguage = 'BackendDevelopment'
 
 console.log(myName)
+  
+   /*OR USING OBJECTS
+    ACCORDING TO THE LECTURE, OBJECTS IS USED TO STORE USERS INFORMATION*/
+
+const user = {
+    myAge: 19,
+    myName : "Taiwo Peace Fiyinfoluwa",
+    myHobby : 'reading, learning',
+    programmingLanguage : "Backend Development"
+
+}
